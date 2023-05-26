683ba86e160bf2ede13d14d8a0c5c435cbf5975f
Dushna Malinda <121780113+DushanMalind@users.noreply.github.com>
2023-05-26 18:35:54 +0530

    img add 1

--- Assignments/Js/SPA/javaScript.js
+++ Assignments/Js/SPA/javaScript.js
@@ -11,3 +11,26 @@ document.getElementById("customers").addEventListener("click",function (){
 });
 
 
+document.getElementById("save").addEventListener("click",function (){
+    let id=document.getElementById("ids").value;
+    let name=document.getElementById("name").value;
+    let address=document.getElementById("address").value;
+    let contact=document.getElementById("contact").value;
+
+
+   let a= document.createElement("td").append(id);
+   let b=document.createElement("td").append(name);
+   let c=document.createElement("td").append(address);
+   let d=document.createElement("td").append(contact);
+
+   let g=document.createElement("tr");
+   e.appendChild(a);
+   e.appendChild(b);
+   e.appendChild(c);
+   e.appendChild(d);
+
+  document.getElementById("tbody").appendChild(g);
+
+
+});
+
