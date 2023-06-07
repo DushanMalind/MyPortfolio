d52d4441644bf345014a7387f7af23b53f89dd54
Dushna Malinda <121780113+DushanMalind@users.noreply.github.com>
2023-06-07 11:58:11 +0530

    InMemory-POS System

---
+++ Assignments/Js/InMemory-POS/javaScript.js
@@ -0,0 +1,17 @@
+/*
+document.getElementById("customer")
+document.getElementById("item").style.display="none";
+document.getElementById("orders").style.display="none";
+document.getElementById("placeOrders").style.display="none";
+
+document.getElementById("customers").addEventListener("click",function (){
+    document.getElementById("customer").style.display="block"
+    document.getElementById("item").style.display="none";
+    document.getElementById("orders").style.display="none";
+    document.getElementById("placeOrders").style.display="none";
+});
+*/
+
+
+
+
